1c2210c1301a0c97af7febfa6f6100e65145e81e
naveenvb560 <naveenvb560@gmail.com>
2022-04-29 00:38:23 +0530

    working on modals

---
+++ src/Utils/utilFunctions.js
@@ -0,0 +1,22 @@
+import Data from '../assets/Data.json'
+
+
+export const getSkillsForProject=(skillsIDs)=> {
+    const skillsList = Data.Skills
+
+    let skillsData = []
+
+    if(Array.isArray(skillsIDs)) {
+
+        if(skillsIDs.length === 0) return false
+        skillsList.forEach(skill => {
+            if (skillsIDs.includes(parseInt(skill.id))) {
+                skillsData.push(skill)
+            }
+        })
+        return skillsData
+    } else {
+        return false
+    }
+
+}
--- src/components/Modals/DetailsModal.js
+++ src/components/Modals/DetailsModal.js
@@ -1,27 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import classes from './DetailsModal.module.css'
+import { getSkillsForProject } from '../../Utils/utilFunctions'
 
 const DetailsModal = (props) => {
     let renderData = null
-
     const details = props.details
     if (details.item === 'project') {
         const project = props.itemData
-        
+        const skillsList = getSkillsForProject(project.techused)
+        console.log(skillsList)
         renderData = <div>
             <h4 className={classes.projectname}>{project.name}</h4>
             <p>{project.details}</p>
             <table>
+                <tbody>
                 <tr>
-                <td>
-                    <h5 className={classes.smallheading}>Technologies used  </h5>
-                </td>
-                <td>
-                <img style={{width: "50%"}} src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/000000/external-technology-new-media-flaticons-lineal-color-flat-icons-3.png"/>
-                </td>
+                    <td>
+                        <h5 className={classes.smallheading}>Technologies used  </h5>
+                    </td>
+                    <td>
+                        <img style={{ width: "50%" }} src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/000000/external-technology-new-media-flaticons-lineal-color-flat-icons-3.png" />
+                    </td>
                 </tr>
+                </tbody>
             </table>
+            <ul>
+                {skillsList.map((skill)=>  <li className={classes.listitems} >{skill.name}</li>)}
+            </ul>
         </div>
     }
     else {
@@ -29,12 +35,12 @@ const DetailsModal = (props) => {
     }
 
     return ReactDOM.createPortal(
-            <div className={classes.modalcontainer}>
-                <div className={classes.modal}>
+        <div className={classes.modalcontainer}>
+            <div className={classes.modal}>
                 <button onClick={props.clear} className={classes.link2}></button>
-                    {renderData}
-                </div>
+                {renderData}
             </div>
+        </div>
         , document.getElementById("modal-root")
     )
 }
