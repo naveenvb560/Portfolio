9227f909969b831ea86de96d94e419166c9c927d
naveenvb560 <naveenvb560@gmail.com>
2022-05-01 01:42:57 +0530

    workingon animations

--- src/components/Pages/Experience.js
+++ src/components/Pages/Experience.js
@@ -4,6 +4,7 @@ import ProjectModal from '../Modals/ProjectModal'
 import classes from '../Pages/Project.module.css'
 import Card from "../UI/Card";
 import {dynamicClassName} from '../../Utils/utilFunctions'
+import wipro from '../../assets/wipro.png'
 
 const Experience =()=> {
 
@@ -14,9 +15,16 @@ return (
     <h3 style={{marginLeft: "10%",  color: "rgb(204 198 198 / 93%)", fontSize: "25px"}}>My Experiences</h3>
     <div className={classes.container}>
         {experienceList.map((experience)=> 
-        <div className={dynamicClassName(experience.id, classes, 3)} key={experience.id}> <Card>
+        <div className={dynamicClassName(experience.id, classes, 3)} key={experience.id}> 
+        <div className={classes.flipcard}>
+        <div className={classes.flipcardinner}>
+          <div className={classes.flipcardfront}><Card>
             <ProjectModal experience={experience}/></Card>
-        </div> )}
+            </div> 
+        <div className={classes.flipcardback}>
+            <img style={{width: "50%", marginTop: "15%", marginLeft: "30%"}} src={wipro} ></img>
+            </div>
+        </div> </div> </div>)}
         </div>
     </div>
 )
