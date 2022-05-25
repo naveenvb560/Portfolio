9ce36c62a6f0f8a2041cbb4da97860194da878bc
Naveen VB <naveenvb560@gmail.com>
2022-05-25 20:00:17 +0530

    working on responsive designs

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import MediaLink from '../SubPages/MediaLinks'
 import classes from './Header.module.css'
 import { NavLink } from "react-router-dom";
@@ -6,20 +6,20 @@ import AOS from "aos"
 import "aos/dist/aos.css"
 
 const Header = () => {
-    const [expanded, setExpanded] = useState(false)
+    const [expanded, setExpanded] = useState(true)
     useEffect(() => {
         AOS.init({
             duration: 2000
         });
     });
-    const activeStyle ={
+    const activeStyle = {
         textAlign: "center",
         color: "var(--secondary-color)",
         fontFamily: "Futura",
         textDecoration: 'none',
     }
 
-    const inactiveStyle ={
+    const inactiveStyle = {
         textAlign: "center",
         color: "var(--tertiary-color)",
         fontFamily: "Futura",
@@ -27,13 +27,13 @@ const Header = () => {
     }
 
 
-    const hamburgerMenuHandler=()=> {
-            setExpanded((previousState)=> {
-                return !previousState
-            })
-        }
+    const hamburgerMenuHandler = () => {
+        setExpanded((previousState) => {
+            return !previousState
+        })
+    }
     return (
-        <div data-aos='zoom-in-up'>
+        <div >
             <div >
                 <a onClick={hamburgerMenuHandler} className={classes.hamburger}>
                     <span className={classes.line}></span>
@@ -41,27 +41,21 @@ const Header = () => {
                     <span className={classes.line}></span>
                 </a>
             </div>
-<div className={expanded ? classes.container : `${classes.container} ${classes.displayhamburger}`}>
-  <div className={classes.item1}>
-  <NavLink style={({ isActive }) =>
-              isActive ? activeStyle : inactiveStyle} to="/">Home</NavLink>
-  </div>
-  <div className={classes.item2}>
-  <NavLink style={({ isActive }) =>
-              isActive ? activeStyle : inactiveStyle} to="Projects">Projects</NavLink>
-  </div>
-  <div className={classes.item3}>
-  <NavLink style={({ isActive }) =>
-              isActive ? activeStyle : inactiveStyle} to="/Experiences">Experiences</NavLink>
-      </div>  
-  <div className={classes.item4}>
-  <NavLink style={({ isActive }) =>
-              isActive ? activeStyle : inactiveStyle} to="Skills">Skills</NavLink></div>
-  <div className={classes.item5}>
-      <MediaLink />
-  </div>
-</div>
-</div>
+            <div className={expanded ? classes.container : `${classes.container} ${classes.displayhamburger}`}>
+                    <div className={classes.navlinks}>
+                        <NavLink className={classes.item} style={({ isActive }) =>
+                            isActive ? activeStyle : inactiveStyle} to="/">Home</NavLink>
+                        <NavLink className={classes.item} style={({ isActive }) =>
+                            isActive ? activeStyle : inactiveStyle} to="Projects">Projects</NavLink>
+                        <NavLink className={classes.item} style={({ isActive }) =>
+                            isActive ? activeStyle : inactiveStyle} to="/Experiences">Experiences</NavLink>
+                        <NavLink className={classes.item} style={({ isActive }) =>
+                            isActive ? activeStyle : inactiveStyle} to="Skills">Skills</NavLink></div>
+                    <div className={classes.medialink}>
+                        <MediaLink />
+                    </div>
+            </div>
+        </div>
     )
 
 }
