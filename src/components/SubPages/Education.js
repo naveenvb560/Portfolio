52a05bf9e41d301202a547a5b1604820bc78345b
Naveen VB <naveenvb560@gmail.com>
2022-05-26 21:57:59 +0530

    testing again

--- src/components/SubPages/Education.js
+++ src/components/SubPages/Education.js
@@ -10,17 +10,17 @@ const Education = () => {
             </div>
             <div className={classes.educationsubcontainer}>
                 <div className={classes.educationchild}>
-                    <p>2016-2019</p>
+                    <p className={classes.year}>2016-2019</p>
                     <h3 className={classes.institutionname}>Church Missionary Society (CMS) College</h3>
                     <p>Bachelor of computer applications</p>
                 </div>
                 <div className={classes.educationchild}>
-                    <p>2014-2016</p>
+                    <p className={classes.year}>2014-2016</p>
                     <h3 className={classes.institutionname}>Sree Kumaramangalam Higher Secondary School, Kumarakom</h3>
                     <p>Higher Secondary ( Plus two )</p>
                 </div>
                 <div className={classes.educationchild}>
-                    <p>2013-2014</p>
+                    <p className={classes.year}>2013-2014</p>
                     <h3 className={classes.institutionname}>Sree Kumaramangalam Higher Secondary School, Kumarakom</h3>
                     <p>S S L C</p>
                 </div>
