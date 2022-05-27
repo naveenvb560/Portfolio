09241d3afb770185cef3a45a5f30d769b55846ad
Naveen VB <naveenvb560@gmail.com>
2022-05-27 21:55:47 +0530

    tow color themes implemented

--- src/components/SubPages/About.js
+++ src/components/SubPages/About.js
@@ -4,12 +4,30 @@ import photo from '../../assets/profile.jpg'
 import Button from "../UI/Button";
 import myData from '../../assets/Data.json';
 import { useNavigate } from "react-router-dom";
+let isDarkTheme= true
 const About = () => {
-
     const navigate = useNavigate()
     const viewProjectHandler = () => {
 
         navigate('Projects')
+    }
+
+    const changeTheme =()=> {
+        if(isDarkTheme) {
+            document.documentElement.style.setProperty('--primary-color',"#e9e8cd");
+            document.documentElement.style.setProperty('--secondary-color',"#161515");
+            document.documentElement.style.setProperty('--tertiary-color',"#0f3b3d");
+            document.documentElement.style.setProperty('--addon-color',"#c4281d");
+            document.documentElement.style.setProperty('--gradient',"#fed9b7");
+            isDarkTheme = false }
+            else {
+                document.documentElement.style.setProperty('--primary-color',"#000401");
+                document.documentElement.style.setProperty('--secondary-color',"#b3d4d4");
+                document.documentElement.style.setProperty('--tertiary-color',"#8D9797");
+                document.documentElement.style.setProperty('--addon-color',"#916f08");
+                document.documentElement.style.setProperty('--gradient',"linear-gradient(to left, #916f081a, #1e1e1eb3)");
+                isDarkTheme = true
+            }
     }
 
     const AboutDescription = myData.About.description
@@ -18,7 +36,7 @@ const About = () => {
 
             <div className={classes.imagecontainer}>
                 <div>
-                    <img className={classes.img} src={photo} alt="profile_pic"></img>
+                    <img onClick={changeTheme} className={classes.img} src={photo} alt="profile_pic"></img>
                 </div>
             </div>
             <div className={classes.detailcontainer}>
